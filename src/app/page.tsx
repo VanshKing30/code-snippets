1e9dbb8ddf9a3cd5ae4f79ee221f299468a77b94
Vansh <vanshwaldeo360@gmail.com>
2025-02-15 19:41:51 +0530

    Made server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,34 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
-import Image from "next/image";
+import { View } from "lucide-react";
 import { ClipboardPlus } from "lucide-react";
+import { prisma } from "@/lib/prisma";
 
+export default async function Home() {
+  const mySnippets = await prisma.snippets.findMany();
+  
 
-export default function Home() {
   return (
     <div>
       <h1 className="font-bold text-4xl">HomePage</h1>
       <div className="flex items-center justify-between">
         <h1>Snippetts</h1>
-        <Link href={"/snippet/new"}> <Button variant="outline"><ClipboardPlus/>New</Button>  </Link>
+        <Link href={"/snippet/new"}>
+          {" "}
+          <Button variant="outline">
+            <ClipboardPlus />
+            New
+          </Button>{" "}
+        </Link>
       </div>
+      {mySnippets.map((single) =>(
+        <div key={single.id} className="flex items-center justify-between bg-gray-200 p-2 rounded-md my-2">
+          <h1>{single.title}</h1>
+          <Link href={`/snippet/${single.id}`}>
+          <Button> <View/>View</Button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
+import {redirect} from "next/navigation";
 
 const CreateSnippet = () => {
+
+    
+    async function createSnippet(formData :FormData){
+        "use server"
+        const title = formData.get("title") as string;
+        const code = formData.get("code") as string;
+
+
+        const snippet = await prisma.snippets.create({
+            data : {
+                title , 
+                code
+            }
+        })
+        console.log(snippet);
+
+        redirect("/")
+
+        
+        
+    }
   return (
-    <div>You can create new snipper here</div>
-  )
-}
+    <form action={createSnippet}>
+      <div>
+        <div>
+          <Label>Title</Label>
+          <Input type="text" name="title" id="title"></Input>
+        </div>
+        <div>
+          <Label>Code</Label>
+          <Textarea name="code" id="code"></Textarea>
+        </div>
+        <Button type="submit" className="my-4">Save</Button>
+      </div>
+    </form>
+  );
+};
 export default CreateSnippet;
