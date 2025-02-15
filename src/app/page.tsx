1fc7fc9e48416166df70132c3f539db5bb43314d
Vansh <vanshwaldeo360@gmail.com>
2025-02-15 16:46:34 +0530

    Initialized Repo

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-black">
+      
+   
+    
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="font-bold text-4xl text-black">
+          Welcome to my Home Page
+          
+        </h1>
+       
+      </div>
+    </div>
+  );
+}
